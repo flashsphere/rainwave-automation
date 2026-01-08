de9a9d837cc8b062aaf77069cfed93ff9b5b2384
Senxian Z <9044823+flashsphere@users.noreply.github.com>
2026-01-08 16:31:12 +0800

    Add icon

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
-  modules: ['@wxt-dev/module-react'],
+  modules: ['@wxt-dev/auto-icons', '@wxt-dev/module-react'],
   manifest: {
     name: "Rainwave automation",
     web_accessible_resources: [{
